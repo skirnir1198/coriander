fc5feccee29429a02494f0b1e16a270c667d8587
miyazakiari <skirnir11981198@gmail.com>
2021-05-02 14:47:33 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("KBOYのFlutter大学"),
+        title: Text("KBOYさんかっこいい"),
       ),
       body: Container(
         width: double.infinity,
